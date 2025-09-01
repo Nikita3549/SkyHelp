16f2b8dc70d28168cd9f996c67972ea6072244e6
Nikita <nikitatsarenko7@gmail.com>
2025-09-01 16:02:47 +0300

    allow generate jwt for agents and partners

--- src/modules/generate-jwt/generate-jwt.controller.ts
+++ src/modules/generate-jwt/generate-jwt.controller.ts
@@ -2,14 +2,14 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { TokenService } from '../token/token.service';
 import { GenerateJwtDto } from './dto/generate-jwt.dto';
 import { JwtAuthGuard } from '../../guards/jwtAuth.guard';
-import { IsAdminGuard } from '../../guards/isAdminGuard';
+import { IsPartnerOrAgentGuard } from '../../guards/isPartnerOrAgentGuard';
 
 @Controller('jwt')
 export class GenerateJwtController {
     constructor(private readonly tokenService: TokenService) {}
 
     @Post()
-    @UseGuards(JwtAuthGuard, IsAdminGuard)
+    @UseGuards(JwtAuthGuard, IsPartnerOrAgentGuard)
     generateJwt(@Body() dto: GenerateJwtDto) {
         const { claimId } = dto;
 
