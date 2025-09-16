f1bf6a0a1fe997750c2b439c73a3a3087398e2af
Nikita <nikitatsarenko7@gmail.com>
2025-09-16 13:49:44 +0300

    update redis module: use default port for redis manually

--- src/modules/redis/redis.service.ts
+++ src/modules/redis/redis.service.ts
@@ -8,6 +8,7 @@ export class RedisService extends Redis {
         const redisConfig = {
             host: process.env.REDIS_HOST,
             password: process.env.REDIS_PASSWORD,
+            port: 6379,
         };
         super(redisConfig);
     }
