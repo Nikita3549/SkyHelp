8894cbee6f0a4fcc724f89b8a5f335f2e14abe74
Nikita <nikitatsarenko7@gmail.com>
2025-10-17 18:37:24 +0300

    chore(claim progress): log send-new-progress-email job.data

--- src/modules/claim/progress/processors/send-new-progress-email.processor.ts
+++ src/modules/claim/progress/processors/send-new-progress-email.processor.ts
@@ -5,19 +5,26 @@ import { NotificationService } from '../../../notification/notification.service'
 import { ISendNewProgressEmailJobData } from '../interfaces/send-new-progress-email-job-data.interface';
 import { ProgressService } from '../progress.service';
 import { ClaimService } from '../../claim.service';
+import { ProgressLogger } from '../progress.logger';
 
 @Processor(SEND_NEW_PROGRESS_EMAIL_QUEUE_KEY)
 export class SendNewProgressEmailProcessor extends WorkerHost {
+    private readonly logger: ProgressLogger;
     constructor(
         private readonly notificationService: NotificationService,
         private readonly progressService: ProgressService,
         private readonly claimService: ClaimService,
     ) {
         super();
+
+        this.logger = new ProgressLogger();
     }
 
     async process(job: Job<ISendNewProgressEmailJobData>) {
         const { progressId, emailData, newClaimStatus } = job.data;
+        this.logger.log(
+            `claimId: ${emailData.claimId} progress job data: ${JSON.stringify(job.data, null, 2)}`,
+        );
 
         const progress = await this.progressService.getProgressById(progressId);
 
