a7f332ddc976431b1ed5801cffe3e24731485b68
Nikita <nikitatsarenko7@gmail.com>
2025-10-30 19:53:33 +0300

    fix: update file paths after refactor

--- src/common/constants/paths/UploadsDirectoryPath.ts
+++ src/common/constants/paths/UploadsDirectoryPath.ts
@@ -1,3 +1,6 @@
 import * as path from 'path';
 
-export const UPLOAD_DIRECTORY_PATH = path.join(__dirname, '../../../uploads');
+export const UPLOAD_DIRECTORY_PATH = path.join(
+    __dirname,
+    '../../../../uploads',
+);
