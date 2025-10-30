543138d768aff8f6e196c24ece052563502186f7
Nikita <nikitatsarenko7@gmail.com>
2025-10-30 14:07:22 +0300

    fix: update file paths after refactor

--- src/common/constants/paths/LettersDirectoryPath.ts
+++ src/common/constants/paths/LettersDirectoryPath.ts
@@ -1,3 +1,3 @@
 import * as path from 'path';
 
-export const LETTERS_DIRECTORY_PATH = path.join(__dirname, '../../letters');
+export const LETTERS_DIRECTORY_PATH = path.join(__dirname, '../../../letters');
