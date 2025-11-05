490a93533eaec2621e469ae33ef7fde1541b3097
Nikita <nikitatsarenko7@gmail.com>
2025-11-05 23:26:17 +0300

    fix(other passengers): handle documentTypes parsing in multipart requests

--- src/modules/claim/other-passenger/dto/upload-other-passenger.dto.ts
+++ src/modules/claim/other-passenger/dto/upload-other-passenger.dto.ts
@@ -1,13 +1,20 @@
 import { IsEnum, IsJWT, IsString } from 'class-validator';
 import { DocumentType } from '@prisma/client';
+import { Transform } from 'class-transformer';
 
 export class UploadOtherPassengerDto {
     @IsString()
     claimId: string;
 
+    @Transform(({ value }) => {
+        try {
+            return JSON.parse(value);
+        } catch {
+            return Array.isArray(value) ? value : [value];
+        }
+    })
     @IsEnum(DocumentType, { each: true })
     documentTypes: DocumentType[];
-
     @IsJWT()
     jwt: string;
 
