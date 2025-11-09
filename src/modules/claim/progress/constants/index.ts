6d244520e0312a290aeda2b827dbee21e22d88e6
Nikita <nikitatsarenko7@gmail.com>
2025-11-09 18:58:20 +0300

    chore(progress): decrease send_new_progress_email_queue_delay to 1 minute

--- src/modules/claim/progress/constants/index.ts
+++ src/modules/claim/progress/constants/index.ts
@@ -6,4 +6,4 @@ export const PROGRESS_NOT_FOUND = 'Progress not found';
 export const SEND_NEW_PROGRESS_EMAIL_QUEUE_KEY =
     'send-new-progress-email-queue';
 
-export const SEND_NEW_PROGRESS_EMAIL_QUEUE_DELAY = isProd() ? 5 * MINUTE : 0;
+export const SEND_NEW_PROGRESS_EMAIL_QUEUE_DELAY = isProd() ? MINUTE : 0;
