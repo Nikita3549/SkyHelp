d734449000d2d3bd281a19a5dc50511ae2db0f36
Nikita <nikitatsarenko7@gmail.com>
2025-11-20 23:18:57 +0300

    fix(assignments): update is old assignment date

--- src/modules/claim/document/document.controller.ts
+++ src/modules/claim/document/document.controller.ts
@@ -110,7 +110,7 @@ export class DocumentController {
                     date: claim.details.date,
                     flightNumber: claim.details.flightNumber,
                 },
-                claim.createdAt <= new Date('2025-10-08'),
+                claim.createdAt <= new Date('2025-10-09'),
             );
         } else {
             assignmentFilePath =
