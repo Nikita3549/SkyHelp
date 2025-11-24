50223ffb55b23ee8611d53a63ba2a47b077ddfb3
Nikita <nikitatsarenko7@gmail.com>
2025-11-24 15:23:26 +0300

    fix(claim): save compensation in customer and other passengers fields when creating claim

--- src/modules/claim/other-passenger/other-passenger.service.ts
+++ src/modules/claim/other-passenger/other-passenger.service.ts
@@ -29,6 +29,7 @@ export class OtherPassengerService {
                     data: {
                         ...p,
                         claimId,
+                        compensation,
                     },
                 }),
             ),
