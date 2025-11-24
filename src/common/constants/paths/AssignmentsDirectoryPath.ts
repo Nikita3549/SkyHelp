0db1c79041df1c0ee28880f890412b0030c60e27
Nikita <nikitatsarenko7@gmail.com>
2025-11-24 15:30:35 +0300

    fix(file paths): use static variables for assets and fonts

---
+++ src/common/constants/paths/AssignmentsDirectoryPath.ts
@@ -0,0 +1,6 @@
+import * as path from 'path';
+
+export const AssignmentsDirectoryPath = path.join(
+    __dirname,
+    '../../../../assets/assignments',
+);
---
+++ src/common/constants/paths/FontsDirectoryPath.ts
@@ -0,0 +1,3 @@
+import * as path from 'path';
+
+export const FontsDirectoryPath = path.join(__dirname, '../../../../fonts');
