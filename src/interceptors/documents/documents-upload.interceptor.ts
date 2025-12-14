223064847d0554d46b9d51ba3c4cd4c74336d3eb
Nikita <nikitatsarenko7@gmail.com>
2025-12-14 12:40:43 +0300

    fix(documents): support utf8 encoded filenames

--- src/interceptors/documents/documents-upload.interceptor.ts
+++ src/interceptors/documents/documents-upload.interceptor.ts
@@ -11,6 +11,10 @@ export function DocumentsUploadInterceptor() {
             storage: diskStorage({
                 destination: UPLOAD_DIRECTORY_PATH,
                 filename: (_req, file, cb) => {
+                    file.originalname = Buffer.from(
+                        file.originalname,
+                        'latin1',
+                    ).toString('utf8');
                     const uniqueSuffix =
                         Date.now() + '-' + Math.round(Math.random() * 1e9);
                     const ext = path.extname(file.originalname);
@@ -18,6 +22,10 @@ export function DocumentsUploadInterceptor() {
                 },
             }),
             fileFilter: (_req, file, cb) => {
+                file.originalname = Buffer.from(
+                    file.originalname,
+                    'latin1',
+                ).toString('utf8');
                 const ext = path.extname(file.originalname).toLowerCase();
                 if (ALLOWED_EXTENSIONS.includes(ext)) {
                     cb(null, true);
