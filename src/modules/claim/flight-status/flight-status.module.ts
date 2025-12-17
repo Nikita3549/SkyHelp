1ea208ce1dbd621b7fb7223bf77baa5e49c5526b
Nikita <nikitatsarenko7@gmail.com>
2025-12-17 17:52:04 +0300

    fix(flight-status): import airline module to prevent build exception

--- src/modules/claim/flight-status/flight-status.module.ts
+++ src/modules/claim/flight-status/flight-status.module.ts
@@ -3,9 +3,10 @@ import { FlightStatusService } from './flight-status.service';
 import { FlightStatusController } from './flight-status.controller';
 import { ClaimModule } from '../claim.module';
 import { FlightModule } from '../../flight/flight.module';
+import { AirlineModule } from '../../airline/airline.module';
 
 @Module({
-    imports: [forwardRef(() => ClaimModule), FlightModule],
+    imports: [forwardRef(() => ClaimModule), FlightModule, AirlineModule],
     providers: [FlightStatusService],
     controllers: [FlightStatusController],
     exports: [FlightStatusService],
