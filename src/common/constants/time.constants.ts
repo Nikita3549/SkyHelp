1c3e27d3a01cba160eeb9e23dd7ce35b0e788676
Nikita <nikitatsarenko7@gmail.com>
2025-12-18 17:30:13 +0300

    chore: correct work time to Moldova

--- src/common/constants/time.constants.ts
+++ src/common/constants/time.constants.ts
@@ -5,5 +5,5 @@ export const HOUR = 60 * MINUTE;
 export const DAY = 24 * HOUR;
 export const DAY_IN_SECONDS = 60 * 60 * 24;
 
-export const WORK_START_HOUR = 9;
-export const WORK_END_HOUR = 17;
+export const WORK_START_HOUR = 6;
+export const WORK_END_HOUR = 14;
