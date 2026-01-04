2d3b074ef32ff668576a7297281c461d7e9830e1
Nikita <nikitatsarenko7@gmail.com>
2026-01-04 22:54:18 +0300

    fix(elastic): connect to elastic properly

--- src/modules/elastic-search/elastic-search.module.ts
+++ src/modules/elastic-search/elastic-search.module.ts
@@ -2,11 +2,18 @@ import { Module, Provider } from '@nestjs/common';
 import { SearchSyncService } from './search-sync.service';
 import { Client } from '@elastic/elasticsearch';
 import { ELASTIC_CLIENT_TOKEN } from './constants/elastic-client.token';
+import * as process from 'process';
 
 const ElasticClient: Provider = {
     provide: ELASTIC_CLIENT_TOKEN,
     useFactory: () => {
-        return new Client({ node: 'http://localhost:9200' });
+        return new Client({
+            node: `http://${process.env.ELASTICSEARCH_HOST!}:${process.env.ELASTICSEARCH_PORT!}`,
+            auth: {
+                username: process.env.ELASTICSEARCH_USER!,
+                password: process.env.ELASTICSEARCH_PASSWORD!,
+            },
+        });
     },
 };
 
